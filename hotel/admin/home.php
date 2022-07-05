ba5139effdf60df994238046d1c84ec611ada741
Yulianabel30 <yulianabella914@gmail.com>
2022-07-05 20:39:17 +0700

    WP Tugas Kelompok 4

---
+++ hotel/admin/home.php
@@ -0,0 +1,2 @@
+<h2 style="text-align: center;text-decoration:underline;">Selamat Datang</h2>
+<!-- <br><center><p>Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a></p></center> -->
